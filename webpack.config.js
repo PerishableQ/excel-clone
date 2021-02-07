ff84cdcc49dc159510d2a67149bec7170d644eb1
PerishableQ <54352261+PerishableQ@users.noreply.github.com>
2021-02-07 18:31:49 +0300

    just a little changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,11 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
     context: path.resolve(__dirname, 'src'),
+    mode: 'development',
     entry: './index.js',
     output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: 'bundle.js'
-    },
-    devServer: {
-        port: 8000,
-        historyApiFallback: true,
-        hot: true,
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
     },
 }
